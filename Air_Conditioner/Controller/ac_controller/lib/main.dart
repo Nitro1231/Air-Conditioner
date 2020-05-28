3c130b2bd45286bae9963c4048d3acd18dba5313
Nitro <nitro0@naver.com>
2020-05-28 13:02:18 +0900

    Layout update.

--- Air_Conditioner/Controller/ac_controller/lib/main.dart
+++ Air_Conditioner/Controller/ac_controller/lib/main.dart
@@ -50,123 +50,220 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          //child: _widgetOptions.elementAt(_selectedIndex),
-          mainAxisAlignment: MainAxisAlignment.start,
+      body: Scrollbar(
+        //mainAxisAlignment: MainAxisAlignment.start,
+        child: ListView(
+          shrinkWrap: true,
           children: <Widget>[
-            ListView(
-              shrinkWrap: true,
+            ExpansionTile(
+                leading: Icon(Icons.timeline),
+                title: Text('Live Status'),
+                children: <Widget>[
+                  GridView.count(
+                    primary: false,
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    padding: const EdgeInsets.all(5),
+                    crossAxisSpacing: 5,
+                    mainAxisSpacing: 5,
+                    children: <Widget>[
+                      Column(children: <Widget>[
+                        Text(
+                          'Outside Temp\n',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                          '$outsideTemp',
+                          style: Theme.of(context).textTheme.headline3,
+                        )
+                      ]),
+                      Column(children: <Widget>[
+                        Text(
+                          'Inside Temp\n',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                          '$insideTemp',
+                          style: Theme.of(context).textTheme.headline3,
+                        )
+                      ]),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Fan Speed\n',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          SizedBox(
+                              height: 150.0,
+                              child: Stack(
+                                children: <Widget>[
+                                  Center(
+                                      child: Container(
+                                          height: 150.0,
+                                          width: 150.0,
+                                          child: CircularProgressIndicator(
+                                            value: 0.5,
+                                            backgroundColor: Colors.black12,
+                                            valueColor:
+                                                AlwaysStoppedAnimation<Color>(
+                                                    Colors.cyan),
+                                            strokeWidth: 7,
+                                          ))),
+                                  Center(
+                                    child: Text("50%",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline4),
+                                  )
+                                ],
+                              )),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'Dial\n',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          SizedBox(
+                              height: 150.0,
+                              child: Stack(
+                                children: <Widget>[
+                                  Center(
+                                      child: Container(
+                                          height: 150.0,
+                                          width: 150.0,
+                                          child: CircularProgressIndicator(
+                                            value: 0.5,
+                                            backgroundColor: Colors.black12,
+                                            valueColor:
+                                                AlwaysStoppedAnimation<Color>(
+                                                    Colors.cyan),
+                                            strokeWidth: 7,
+                                          ))),
+                                  Center(
+                                    child: Text("50%",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline4),
+                                  )
+                                ],
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
+                  Text(
+                    "\nLast updated: sec ago..\n"
+                  )
+                ]),
+            ExpansionTile(
+              leading: Icon(Icons.phone_android),
+              title: Text('Mode'),
               children: <Widget>[
-                ExpansionTile(
-                  leading: Icon(Icons.timeline),
-                  title: Text('Live Status'),
-                  children: <Widget>[
-                    Text(
-                      '$outsideTemp',
-                      style: Theme.of(context).textTheme.headline3,
-                    ),
-                    Text(
-                      'Outside Temperature\n\n',
-                      style: Theme.of(context).textTheme.headline6,
-                      textAlign: TextAlign.left,
-                    ),
-                    Text(
-                      '$insideTemp',
-                      style: Theme.of(context).textTheme.headline3,
-                    ),
-                    Text(
-                      'Inside Temperature\n\n',
-                      textAlign: TextAlign.left,
-                      style: Theme.of(context).textTheme.headline6,
-                    )
-                  ],
+                ListTile(
+                  title: const Text('Auto'),
+                  leading: Radio(
+                    value: mode.Auto,
+                    groupValue: controlMode,
+                    activeColor: Colors.cyan,
+                    onChanged: (mode value) {
+                      setState(() {
+                        controlMode = value;
+                        controlVisibility = false;
+                      });
+                    },
+                  ),
                 ),
-                ExpansionTile(
-                  leading: Icon(Icons.phone_android),
-                  title: Text('Mode'),
-                  children: <Widget>[
-                    ListTile(
-                      title: const Text('Auto'),
-                      leading: Radio(
-                        value: mode.Auto,
-                        groupValue: controlMode,
-                        activeColor: Colors.cyan,
-                        onChanged: (mode value) {
-                          setState(() {
-                            controlMode = value;
-                            controlVisibility = false;
-                          });
-                        },
-                      ),
-                    ),
-                    ListTile(
-                      title: const Text('Dial'),
-                      leading: Radio(
-                        value: mode.Dial,
-                        groupValue: controlMode,
-                        activeColor: Colors.cyan,
-                        onChanged: (mode value) {
-                          setState(() {
-                            controlMode = value;
-                            controlVisibility = false;
-                          });
-                        },
-                      ),
-                    ),
-                    ListTile(
-                      title: const Text('Bluetooth'),
-                      leading: Radio(
-                        value: mode.Bluetooth,
-                        groupValue: controlMode,
-                        activeColor: Colors.cyan,
-                        onChanged: (mode value) {
-                          setState(() {
-                            controlMode = value;
-                            controlVisibility = true;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
+                ListTile(
+                  title: const Text('Dial'),
+                  leading: Radio(
+                    value: mode.Dial,
+                    groupValue: controlMode,
+                    activeColor: Colors.cyan,
+                    onChanged: (mode value) {
+                      setState(() {
+                        controlMode = value;
+                        controlVisibility = false;
+                      });
+                    },
+                  ),
                 ),
-                AnimatedOpacity(
-                    opacity: controlVisibility ? 1.0 : 0.4,
-                    duration: Duration(milliseconds: 500),
-                    child: AbsorbPointer(
-                        absorbing: !controlVisibility,
-                        child: ExpansionTile(
-                          leading: Icon(Icons.computer),
-                          title: Text('Control'),
-                          children: <Widget>[
-                            SliderTheme(
-                              data: SliderTheme.of(context).copyWith(
-                                activeTrackColor: Colors.cyan[700],
-                                inactiveTrackColor: Colors.black54,
-                                trackShape: RectangularSliderTrackShape(),
-                                trackHeight: 4.0,
-                                thumbColor: Colors.cyan,
-                                thumbShape: RoundSliderThumbShape(
-                                    enabledThumbRadius: 12.0),
-                                overlayColor: Colors.cyan.withAlpha(32),
-                                overlayShape: RoundSliderOverlayShape(
-                                    overlayRadius: 28.0),
-                              ),
-                              child: Slider(
-                                min: 0,
-                                max: 255,
-                                value: fanSpeed,
-                                onChanged: (value) {
-                                  setState(() {
-                                    fanSpeed = value;
-                                  });
-                                },
-                              ),
-                            ),
-                          ],
-                        )))
+                ListTile(
+                  title: const Text('Bluetooth'),
+                  leading: Radio(
+                    value: mode.Bluetooth,
+                    groupValue: controlMode,
+                    activeColor: Colors.cyan,
+                    onChanged: (mode value) {
+                      setState(() {
+                        controlMode = value;
+                        controlVisibility = true;
+                      });
+                    },
+                  ),
+                ),
               ],
             ),
+            AnimatedOpacity(
+                opacity: controlVisibility ? 1.0 : 0.4,
+                duration: Duration(milliseconds: 500),
+                child: AbsorbPointer(
+                    absorbing: !controlVisibility,
+                    child: ExpansionTile(
+                      leading: Icon(Icons.computer),
+                      title: Text('Control'),
+                      children: <Widget>[
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            activeTrackColor: Colors.cyan[700],
+                            inactiveTrackColor: Colors.black54,
+                            trackShape: RectangularSliderTrackShape(),
+                            trackHeight: 4.0,
+                            thumbColor: Colors.cyan,
+                            thumbShape:
+                                RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                            overlayColor: Colors.cyan.withAlpha(32),
+                            overlayShape:
+                                RoundSliderOverlayShape(overlayRadius: 28.0),
+                          ),
+                          child: Slider(
+                            min: 0,
+                            max: 255,
+                            value: fanSpeed,
+                            onChanged: (value) {
+                              setState(() {
+                                fanSpeed = value;
+                              });
+                            },
+                          ),
+                        ),
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            activeTrackColor: Colors.cyan[700],
+                            inactiveTrackColor: Colors.black54,
+                            trackShape: RectangularSliderTrackShape(),
+                            trackHeight: 4.0,
+                            thumbColor: Colors.cyan,
+                            thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                            overlayColor: Colors.cyan.withAlpha(32),
+                            overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 28.0),
+                          ),
+                          child: Slider(
+                            min: 0,
+                            max: 255,
+                            value: fanSpeed,
+                            onChanged: (value) {
+                              setState(() {
+                                fanSpeed = value;
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    )))
           ],
         ),
       ),
